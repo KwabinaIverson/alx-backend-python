01c14944ab5bc3727a006d7dac4461ac6765716a
Benjmain Atsu <Bredrenbonish@gmail.com>
2024-03-19 15:55:37 +0000

    A coroutine function that loops 10 times, each time asynchronously awaits for 1 second then yield a random number between 0 and 10.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ import random
 
 
 async def async_generator():
-"""
-An asynchronous generator that yields random numbers
-10 times with 1 second delay.
-"""
+    """
+    An asynchronous generator that yields random numbers
+    10 times with 1 second delay.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
