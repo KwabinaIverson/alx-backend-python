55033b990e490a0ad9095cf57482c5ef33dddad7
Benjmain Atsu <Bredrenbonish@gmail.com>
2024-04-07 03:06:42 +0000

    method to test that the method returns what it is supposed to.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """
     A test case class for the access_nested_map function.
@@ -16,18 +17,20 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    
     def test_access_nest_map(self, nested_map, path, expected_result):
         """
         Test the access_nested_map function with various inputs.
-        
+
         Arguments:
             - nested_map: A dictionary representing the nested map.
-            - path: A tuple representing the path to access the value in the nested map.
-            - expected_result: The expected result after accessing the nested value.
-        
+            - path: A tuple representing the path to access
+                the value in the nested map.
+            - expected_result: The expected result after
+                accessing the nested value.
+
         This method tests the access_nested_map function by providing
         different nested maps and paths.
-        It compares the result obtained from the function with the expected result.
+        It compares the result obtained from the function
+        with the expected result.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
